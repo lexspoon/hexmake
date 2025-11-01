93ef4a8eb6c125f9eec96aa35e0525c445783111
Lex Spoon <lex@lexspoon.org>
2025-11-01 18:35:47 -0400

    Remove Scala version

--- scala/src/hexmake/ast/HexSettings.scala
+++
@@ -1,6 +0,0 @@
-package hexmake.ast
-
-case class HexSettings(
-    /** The number of concurrent executions to run */
-    concurrency: Int = 8
-)
--- scala/test/src/hexmake/util/HexPaths.scala
+++
@@ -1,9 +0,0 @@
-package hexmake.util
-
-import hexmake.ast.HexPath
-
-import scala.collection.immutable.ArraySeq
-
-object HexPaths:
-  def hexPaths(paths: String*): ArraySeq[HexPath] =
-    ArraySeq.from(paths).map(p => HexPath(p))
